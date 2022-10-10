b4711fcf219c72968c651862ea33bb9211eb5297
hanani8 <hananidasilva@gmail.com>
2022-10-10 14:02:33 +0530

    Removed OnMount

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,8 @@
 	 */
 	export let data;
 
-	let stories = [];
-
-	onMount(async () => {
-		const url = 'https://hacker-news.firebaseio.com/v0/topstories.json';
-		const FetchTopStories = await fetch(url);
-		const TopStoriesIDs = await FetchTopStories.json();
-		let Stories = [];
-		for (let i = 0; i < 10; i++) {
-			const story_url = `https://hacker-news.firebaseio.com/v0/item/${TopStoriesIDs[i]}.json`;
-			const FetchTopStory = await fetch(story_url);
-			const TopStory = await FetchTopStory.json();
-			Stories.push(TopStory);
-		}
-
-		stories = Stories;
-	});
 </script>
 
-<div>
-	{#each stories as story}
-		{story}
-	{/each}
-</div>
 
 <table>
 	<tbody>
